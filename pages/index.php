fa7168dc7fd0d6954f9716e3662cc666c8e3f7b0
MrSnakie <petitloulou96@free.fr>
2017-04-07 14:12:19 +0200

    Initialize all files - MVC architecture
    Note: the login system is not the right one so it does not work

---
+++ pages/index.php
@@ -0,0 +1,5 @@
+<div class="row">
+    <div class="col-md-12 alert alert-info">
+        <strong>Informations:</strong> il n'y a rien à voir ici, connectez vous en administrateur pour accéder à du contenu.
+    </div>
+</div>
